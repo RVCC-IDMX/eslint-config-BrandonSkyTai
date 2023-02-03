1ef53030675900edadf3faddf313a48241f0a97c
BrandonSkyTai <bt325sky@gmail.com>
2023-02-02 23:33:30 -0500

    Fix foo.js

--- foo.js
+++ foo.js
@@ -1,24 +1,4 @@
-   /*
-   
-   whoa!    Hold your horses! This is badly formatted code.
-                       
-   
-*/
+/* whoa! Hold your horses! This is badly formatted code. */
 
-
-
-       var message =      "Hello World"
-
-
-
-
-
-
-
-
-console.log(message)            // this should not show as an error
-
-
-
-
-
+const message = 'Hello World';
+console.log(message); // this should not show as an error
